fec46943e4ebcb37bbb36930fc4b4495140f41fd
marsnebulasoup <marsnebulasoup@gmail.com>
2021-08-17 17:00:37 -0500

    Added sample code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,68 +1,94 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
+    <ion-content class="ion-padding">
+      <ion-button @click="toggleInfiniteScroll" expand="block">
+        Toggle Infinite Scroll
+      </ion-button>
+
+      <ion-list>
+        <ion-item v-for="item in items" :key="item">
+          <ion-label>{{ item }}</ion-label>
+        </ion-item>
+      </ion-list>
+
+      <ion-infinite-scroll
+        @ionInfinite="loadData($event)"
+        threshold="100px"
+        id="infinite-scroll"
+        :disabled="isDisabled"
+      >
+        <ion-infinite-scroll-content
+          loading-spinner="bubbles"
+          loading-text="Loading more data..."
+        >
+        </ion-infinite-scroll-content>
+      </ion-infinite-scroll>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import {
+  IonButton,
+  IonContent,
+  IonInfiniteScroll,
+  IonInfiniteScrollContent,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonPage,
+} from "@ionic/vue";
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
-  name: 'Home',
+  name: "Home",
   components: {
+    IonButton,
     IonContent,
-    IonHeader,
+    IonInfiniteScroll,
+    IonInfiniteScrollContent,
+    IonItem,
+    IonLabel,
+    IonList,
     IonPage,
-    IonTitle,
-    IonToolbar
-  }
+  },
+  setup() {
+    const isDisabled = ref(false);
+    const toggleInfiniteScroll = () => {
+      isDisabled.value = !isDisabled.value;
+    };
+    const items = ref<number[]>([]);
+    const pushData = () => {
+      const max = items.value.length + 20;
+      const min = max - 20;
+      for (let i = min; i < max; i++) {
+        items.value.push(i);
+      }
+    };
+
+    //               👇👇👇👇 👈 set to any to get rid of errors  
+    const loadData = (ev: any) => {
+      setTimeout(() => {
+        pushData();
+        console.log("Loaded data");
+        ev.target.complete();
+
+        // App logic to determine if all data is loaded
+        // and disable the infinite scroll
+        if (items.value.length == 1000) {
+          ev.target.disabled = true;
+        }
+      }, 500);
+    };
+
+    pushData();
+
+    return {
+      isDisabled,
+      toggleInfiniteScroll,
+      loadData,
+      items,
+    };
+  },
 });
 </script>
-
-<style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
-}
-</style>
